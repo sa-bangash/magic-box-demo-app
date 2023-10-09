e0af16f865834b5c7d0c995998ea7276d97fd47d
shahid ahmad <shahid.bangash@hotmail.com>
2023-10-09 06:59:23 +0500

    adding own toolbar and logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,RouterModule],
 })
 export class AppComponent {
   constructor() {}
